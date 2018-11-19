ea6ac4ef2f2665b73cc45533e3fa3070c39bd78a
Ryan Glazer <rglazer003@gmail.com>
2018-11-19 12:30:29 -0600

    Starting javascript and exercises

---
+++ js/external.js
@@ -0,0 +1,4 @@
+console.log ("Hello from external javascript");
+alert ("Welcome to my website");
+var favColor = prompt ("What is your favorite color?");
+alert (favColor + ' is my favorite color too.');
