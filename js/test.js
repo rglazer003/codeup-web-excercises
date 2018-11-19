8f04b7b82f15b3fad6001e324e244f83a2c2705b
Ryan Glazer <rglazer003@gmail.com>
2018-11-19 16:19:27 -0600

    HTML and JS for testing future JS

---
+++ js/test.js
@@ -0,0 +1,20 @@
+var result = 0;
+
+function addit(num1, num2) {
+    result = num1 + num2;
+    return result;
+}
+
+addit(3, 4);
+
+var number = 0;
+function incrementGlobal() {
+  var result2 = number + 1;
+  return result2;
+
+}
+
+var myName = "Dale";
+function nameChange(newName){
+    myName = newName
+}
