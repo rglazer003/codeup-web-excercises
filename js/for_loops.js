75ef0a7267f01e1134e7993347c3c20444110761
Ryan Glazer <rglazer003@gmail.com>
2018-11-28 11:59:11 -0600

    Changed pyramid loop

--- js/for_loops.js
+++ js/for_loops.js
@@ -16,37 +16,14 @@ for (var j=1; j<=10; j++){
         console.log(someNum + " is odd");
     }
 }
-for (var p=1; p<10; p++){
-    if (p === 1){
-        console.log(1)
+for (var i = 1; i < 10; i++) {
+    var  message = "";
+    var length = 0;
+    while (length < i ) {
+        message = message + i;
+        length++;
     }
-    else if (p === 2){
-        console.log(22);
-    }
-    else if (p === 3){
-        console.log(333);
-    }
-    else if (p === 4){
-        console.log(4444);
-    }
-    else if (p === 5){
-        console.log(55555);
-    }
-    else if (p === 6){
-        console.log (666666)
-    }
-    else if (p === 7){
-        console.log (7777777);
-    }
-    else if (p === 8){
-        console.log (88888888);
-    }
-    else if (p === 9){
-        console.log (999999999);
-    }
-    else {
-        break;
-    }
+    console.log(message)
 }
 for (var d=100; d>4; d=d-5){
     console.log(d);
