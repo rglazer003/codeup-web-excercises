f3d0fdc44bc25123f103413128d3a7ca58ca0da4
Ryan Glazer <rglazer003@gmail.com>
2018-11-28 11:37:03 -0600

    Finished loops exercise

---
+++ js/break_and_continue.js
@@ -0,0 +1,23 @@
+var num = prompt ("Enter an odd number between 1 and 50");
+num = Number(num);
+while (0===0){
+    if (num%2===0 || num>50){
+        num = prompt ("Enter another number");
+        num = Number(num)
+    }
+    else {
+        break;
+    }
+}
+console.log (num + " is odd and between 1 and 50");
+for (var i =1; i<50; i++){
+    if (i%2===0){
+        continue;
+    }
+    if (i === num){
+        console.log ("Skipping "+i);
+    }
+    else{
+        console.log (i+" is an odd number")
+    }
+}
