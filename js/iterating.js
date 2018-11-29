c770355435abb783286043fa32898367379371ff
Ryan Glazer <rglazer003@gmail.com>
2018-11-29 14:31:48 -0600

    Added manual index calls for names array as well as the for loop method

--- js/iterating.js
+++ js/iterating.js
@@ -18,15 +18,19 @@
      * Create log statements that will print each of the names individually by
      * accessing each element's index.
      */
-    for (var i = 0; i < names.length; i++){
-        console.log(names[i]);
-    }
+    console.log(names[0]);
+    console.log(names[1]);
+    console.log(names[2]);
+    console.log(names[3]);
     /**
      * TODO:
      * Write some code that uses a for loop to log every item in the names
      * array.
      */
-    //Did that in the above part
+    console.log("Now with a for loop:");
+    for (var i = 0; i < names.length; i++){
+        console.log(names[i]);
+    }
     /**
      * TODO:
      * Refactor your above code to use a `forEach` loop
