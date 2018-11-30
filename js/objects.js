46c22f38270d3ed773e8a8a0e261cf63a0890090
Ryan Glazer <rglazer003@gmail.com>
2018-11-30 16:16:40 -0600

    Still working

--- js/objects.js
+++ js/objects.js
@@ -1,51 +1,67 @@
 (function() {
     "use strict";
 
-    /**
-     * TODO:
-     * Create an object with firstName and lastName properties that are strings
-     * with your first and last name. Store this object in a variable named
-     * `person`.
-     *
-     * Example:
-     *  > console.log(person.firstName) // "Rick"
-     *  > console.log(person.lastName) // "Sanchez"
-     */
+    // /**
+    //  * TODO:
+    //  * Create an object with firstName and lastName properties that are strings
+    //  * with your first and last name. Store this object in a variable named
+    //  * `person`.
+    //  *
+    //  * Example:
+    //  *  > console.log(person.firstName) // "Rick"
+    //  *  > console.log(person.lastName) // "Sanchez"
+    //  */
     var person = {
         firstName: "Ryan",
-        lastName: "Glazer"
+        lastName: "Glazer",
+        sayHello: function () {
+            console.log("Hello "+ this.firstName + " " + this.lastName)
+        }
     };
     console.log(person.firstName);
     console.log(person.lastName);
-    /**
-     * TODO:
-     * Add a sayHello method to the person object that returns a greeting using
-     * the firstName and lastName properties.
-     * console.log the returned message to check your work
-     *
-     * Example
-     * > console.log(person.sayHello()) // "Hello from Rick Sanchez!"
-     */
+    person.sayHello();
+    // /**
+    //  * TODO:
+    //  * Add a sayHello method to the person object that returns a greeting using
+    //  * the firstName and lastName properties.
+    //  * console.log the returned message to check your work
+    //  *
+    //  * Example
+    //  * > console.log(person.sayHello()) // "Hello from Rick Sanchez!"
+    //  */
 
-    /** TODO:
-     * HEB has an offer for the shoppers that buy products amounting to
-     * more than $200. If a shopper spends more than $200, they get a 12%
-     * discount. Write a JS program, using conditionals, that logs to the
-     * browser, how much Ryan, Cameron and George need to pay. We know that
-     * Cameron bought $180, Ryan $250 and George $320. Your program will have to
-     * display a line with the name of the person, the amount before the
-     * discount, the discount, if any, and the amount after the discount.
-     *
-     * Uncomment the lines below to create an array of objects where each object
-     * represents one shopper. Use a foreach loop to iterate through the array,
-     * and console.log the relevant messages for each person
-     */
+    // /** TODO:
+    //  * HEB has an offer for the shoppers that buy products amounting to
+    //  * more than $200. If a shopper spends more than $200, they get a 12%
+    //  * discount. Write a JS program, using conditionals, that logs to the
+    //  * browser, how much Ryan, Cameron and George need to pay. We know that
+    //  * Cameron bought $180, Ryan $250 and George $320. Your program will have to
+    //  * display a line with the name of the person, the amount before the
+    //  * discount, the discount, if any, and the amount after the discount.
+    //  *
+    //  * Uncomment the lines below to create an array of objects where each object
+    //  * represents one shopper. Use a foreach loop to iterate through the array,
+    //  * and console.log the relevant messages for each person
+    //  */
 
-    // var shoppers = [
-    //     {name: 'Cameron', amount: 180},
-    //     {name: 'Ryan', amount: 250},
-    //     {name: 'George', amount: 320}
-    // ];
+    var shoppers = [
+        {name: 'Cameron', amount: 180},
+        {name: 'Ryan', amount: 250},
+        {name: 'George', amount: 320}
+    ];
+shoppers.forEach(function (shopper) {
+    if (shopper.amount >= 200) {
+        var discount = shopper.amount * .12;
+        console.log("Hello " + shopper.name + " your total before discount is " + shopper.amount + " your discount is " + discount + " your total amount is "+ (shopper.amount-discount))
+    }
+    else {
+        console.log("Hello " + shopper.name + " your total of " + shopper.amount + " does not qualify for a discount.")
+    }
+});
+
+
+
 
     /** TODO:
      * Create an array of objects that represent books and store it in a
